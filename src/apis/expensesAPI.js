263158ba10f33bcfafe9ebdb9140826b47046256
Bruno Krebs <bruno.krebs@auth0.com>
2019-07-22 20:44:05 -0300

    renaming scopes;

--- src/apis/expensesAPI.js
+++ src/apis/expensesAPI.js
@@ -8,12 +8,12 @@ const router = express.Router();
 router.use(checkAccessToken(process.env.ISSUER, process.env.EXPENSE_API));
 
 // endpoint to return all expense reports
-router.get('/', checkPermission('read:reports'), async (req, res) => {
+router.get('/', checkPermission('read:expenses'), async (req, res) => {
   res.send(await getExpenses());
 });
 
 // endpoint to insert new expense report
-router.post('/', checkPermission('create:report'), async (req, res) => {
+router.post('/', checkPermission('create:expenses'), async (req, res) => {
   const newExpense = req.body;
   await insertExpense(newExpense);
   res.send({ message: 'Expense report inserted.' });
--- src/apis/vacationsAPI.js
+++ src/apis/vacationsAPI.js
@@ -8,12 +8,12 @@ const router = express.Router();
 router.use(checkAccessToken(process.env.ISSUER, process.env.VACATION_API));
 
 // endpoint to return all vacation requests
-router.get('/', checkPermission('read:requests'), async (req, res) => {
+router.get('/', checkPermission('read:vacations'), async (req, res) => {
   res.send(await getVacations());
 });
 
 // endpoint to insert new vacation request
-router.post('/', checkPermission('create:request'), async (req, res) => {
+router.post('/', checkPermission('create:vacations'), async (req, res) => {
   const newVacation = req.body;
   await insertVacation(newVacation);
   res.send({ message: 'Vacation request inserted.' });
